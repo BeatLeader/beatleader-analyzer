078dc1bd6742034e0013ff0fd67251fc06730777
Loloppe <LaurentBlaisCote@outlook.com>
2023-11-05 21:34:19 -0500

    Ratings class

--- beatleader-analyzer/Analyze.cs
+++ beatleader-analyzer/Analyze.cs
@@ -1,4 +1,4 @@
-﻿using Analyzer.BeatmapScanner;
+﻿using beatleader_analyzer.BeatmapScanner.Data;
 using beatleader_parser;
 using Parser.Map;
 using Parser.Map.Difficulty.V3.Base;
@@ -11,8 +11,12 @@ namespace beatleader_analyzer
 {
     public class Analyze
     {
-        public static List<double> GetDataFromPathOne(string folderPath, string characteristic, string difficulty, float timescale = 1)
+        public static List<Ratings> Ratings = new();
+
+        public static List<Ratings> GetDataFromPathOne(string folderPath, string characteristic, string difficulty, float timescale = 1)
         {
+            Ratings = new();
+
             try
             {
                 Parse.TryLoadPath(folderPath);
@@ -20,112 +24,124 @@ namespace beatleader_analyzer
                 var diff = map.Difficulties.FirstOrDefault(d => d.Difficulty == difficulty && d.Characteristic == characteristic);
                 if (diff.Data.colorNotes.Count >= 20)
                 {
-                    return BeatmapScanner.Analyzer(diff.Data.colorNotes, diff.Data.burstSliders, diff.Data.bombNotes, diff.Data.obstacles, map.Info._beatsPerMinute * timescale);
+                    Ratings.Add(new(characteristic, difficulty, Analyzer.BeatmapScanner.BeatmapScanner.Analyzer(diff.Data.colorNotes, diff.Data.burstSliders, diff.Data.bombNotes, diff.Data.obstacles, map.Info._beatsPerMinute * timescale)));
+                    return Ratings;
                 }
                 else
                 {
-                    return new();
+                    return null;
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                return new();
+                return null;
             }
         }
 
-        public static List<List<double>> GetDataFromPathAll(string folderPath, float timescale = 1)
+        public static List<Ratings> GetDataFromPathAll(string folderPath, float timescale = 1)
         {
+            Ratings = new();
+
             try
             {
                 Parse.TryLoadPath(folderPath);
                 var map = Parse.GetBeatmap();
 
-                List<List<double>> output = new();
-
                 foreach (var difficulty in map.Difficulties)
                 {
                     if (difficulty.Data.colorNotes.Count >= 20)
                     {
-                        output.Add(new());
-                        output[output.Count - 1] = BeatmapScanner.Analyzer(difficulty.Data.colorNotes, difficulty.Data.burstSliders, difficulty.Data.bombNotes, difficulty.Data.obstacles, map.Info._beatsPerMinute * timescale);
+                        Ratings.Add(new(difficulty.Characteristic, difficulty.Difficulty, Analyzer.BeatmapScanner.BeatmapScanner.Analyzer(difficulty.Data.colorNotes, difficulty.Data.burstSliders, difficulty.Data.bombNotes, difficulty.Data.obstacles, map.Info._beatsPerMinute * timescale)));
                     }
                 }
-                return output;
+                return Ratings;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                return new();
+                return null;
             }
         }
 
-        public static List<List<double>> GetDataFromZip(MemoryStream zip, float timescale = 1)
+        public static List<Ratings> GetDataFromZip(MemoryStream zip, float timescale = 1)
         {
+            Ratings = new();
+
             try
             {
                 Parse.TryLoadZip(zip);
                 var map = Parse.GetBeatmap();
-                List<List<double>> output = new();
 
                 foreach (var difficulty in map.Difficulties)
                 {
                     if (difficulty.Data.colorNotes.Count >= 20)
                     {
-                        output.Add(new());
-                        output[output.Count - 1] = BeatmapScanner.Analyzer(difficulty.Data.colorNotes, difficulty.Data.burstSliders, difficulty.Data.bombNotes, difficulty.Data.obstacles, map.Info._beatsPerMinute * timescale);
+                        Ratings.Add(new(difficulty.Characteristic, difficulty.Difficulty, Analyzer.BeatmapScanner.BeatmapScanner.Analyzer(difficulty.Data.colorNotes, difficulty.Data.burstSliders, difficulty.Data.bombNotes, difficulty.Data.obstacles, map.Info._beatsPerMinute * timescale)));
                     }
                 }
-                return output;
+                return Ratings;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                return new();
+                return null;
             }
         }
 
-        public static List<double> GetDataOneDiff(DifficultyV3 difficulty, float bpm, float timescale = 1)
+        public static List<Ratings> GetDataOneDiff(DifficultyV3 diff, string characteristic, string difficulty, float bpm, float timescale = 1)
         {
+            Ratings = new();
+
             try
             {
-                if (difficulty.colorNotes.Count >= 20)
+                if (diff.colorNotes.Count >= 20)
                 {
-                    return BeatmapScanner.Analyzer(difficulty.colorNotes, difficulty.burstSliders, difficulty.bombNotes, difficulty.obstacles, bpm * timescale);
+                    Ratings.Add(new(characteristic, difficulty, Analyzer.BeatmapScanner.BeatmapScanner.Analyzer(diff.colorNotes, diff.burstSliders, diff.bombNotes, diff.obstacles, bpm * timescale)));
+                    return Ratings;
                 }
                 else
                 {
-                    return new();
+                    return null;
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                return new();
+                return null;
             }
         }
 
-        public static List<List<double>> GetDataAllDiff(BeatmapV3 beatmap, float timescale = 1)
+        public static List<Ratings> GetDataAllDiff(BeatmapV3 beatmap, float timescale = 1)
         {
+            Ratings = new();
+
             try
             {
-                List<List<double>> output = new();
-
                 foreach (var difficulty in beatmap.Difficulties)
                 {
                     if (difficulty.Data.colorNotes.Count >= 20)
                     {
-                        output.Add(new());
-                        output[output.Count - 1] = BeatmapScanner.Analyzer(difficulty.Data.colorNotes, difficulty.Data.burstSliders, difficulty.Data.bombNotes, difficulty.Data.obstacles, beatmap.Info._beatsPerMinute * timescale);
+                        Ratings.Add(new(difficulty.Characteristic, difficulty.Difficulty, Analyzer.BeatmapScanner.BeatmapScanner.Analyzer(difficulty.Data.colorNotes, difficulty.Data.burstSliders, difficulty.Data.bombNotes, difficulty.Data.obstacles, beatmap.Info._beatsPerMinute * timescale)));
                     }
                 }
-                return output;
+                return Ratings;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                return new();
+                return null;
             }
+        }
+
+        public static Ratings GetRating(string characteristic, string difficulty)
+        {
+            return Ratings.FirstOrDefault(x => x.Characteristic == characteristic && x.Difficulty == difficulty);
+        }
+
+        public static List<Ratings> GetRatings()
+        {
+            return Ratings;
         }
     }
 }
