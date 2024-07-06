b3b52815d686a1669db4171b3677e484c0ae90c4
Loloppe <LaurentBlaisCote@outlook.com>
2024-07-06 01:50:11 -0400

    Removing Linear and Multi from Ratings

--- beatleader-analyzer/BeatmapScanner/Data/Ratings.cs
+++ beatleader-analyzer/BeatmapScanner/Data/Ratings.cs
@@ -9,8 +9,6 @@ namespace beatleader_analyzer.BeatmapScanner.Data
         public double Pass { get; set; }
         public double Tech { get; set; }
         public double Nerf { get; set; }
-        public double Linear { get; set; }
-        public double Multi { get; set; }
 
         public Ratings(string characteristic, string difficulty, List<double> ratings)
         {
@@ -19,8 +17,6 @@ namespace beatleader_analyzer.BeatmapScanner.Data
             Pass = ratings[0];
             Tech = ratings[1];
             Nerf = ratings[2];
-            Linear = ratings[3];
-            Multi = ratings[4];
         }
     }
 }
